a2ae83593c544390340f02eb6340b4b708b4da09
Pavel Zagrebelin <pavel@zagrebelin.ru>
2017-12-11 17:48:13 +0500

    day10

---
+++ 10/d-10.py
@@ -0,0 +1,20 @@
+from aocframe import AoCFramework
+
+
+class Part1(AoCFramework):
+    def go(self, data: str):
+        data = list(range(256))
+        steps = list(map(int, str))
+        position = 0
+        seek = 0
+        for step in steps:
+            pass
+
+
+class Part2(AoCFramework):
+    def go(self, data: str):
+        ...
+
+
+Part1()
+Part2()
